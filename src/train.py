Fixed issues with calculation of metrics for multiclass. Changes to model.

src/train.py
@@ -6,7 +6,7 @@ import numpy as np
 from imblearn.over_sampling import RandomOverSampler
 from math import ceil
 import datetime
-from tensorflow.keras.metrics import BinaryAccuracy, Precision, Recall, AUC
+from tensorflow.keras.metrics import BinaryAccuracy, CategoricalAccuracy, Precision, Recall, AUC
 from tensorflow.keras.models import save_model
 from tensorflow.keras.callbacks import EarlyStopping, TensorBoard, ReduceLROnPlateau
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -28,7 +28,6 @@ def get_class_weights(histogram, class_multiplier=None):
     class_weight = {i: weights[i] for i in range(len(histogram))}
     if class_multiplier is not None:
         class_weight = [class_weight[i] * class_multiplier[i] for i in range(len(histogram))]
-    class_weight[0] *= 3
     print("Class weights: ", class_weight)
     return class_weight
 
@@ -48,27 +47,15 @@ def random_minority_oversample(train_set):
     return train_set_resampled
 
 
-def train_model(cfg, data, model, callbacks, verbose=1):
+def train_model(cfg, data, callbacks, verbose=1):
     '''
     Train a and evaluate model on given data.
     :param cfg: Project config (from config.yml)
     :param data: dict of partitioned dataset
-    :param model: Keras model to train
     :param callbacks: list of callbacks for Keras model
     :param verbose: Verbosity mode to pass to model.fit_generator()
     :return: Trained model and associated performance metrics on the test set
     '''
-
-    # Apply class imbalance strategy. We have many more X-rays negative for COVID-19 than positive.
-    histogram = np.bincount(data['TRAIN']['label'].astype(int))
-    class_weight = None
-    class_multiplier = None
-    if cfg['TRAIN']['CLASS_MODE'] == 'multiclass':
-        class_multiplier = cfg['TRAIN']['CLASS_MULTIPLIER']
-    if cfg['TRAIN']['IMB_STRATEGY'] == 'class_weight':
-        class_weight = get_class_weights(histogram, class_multiplier)
-    else:
-        data['TRAIN'] = random_minority_oversample(data['TRAIN'])
 
     # Create ImageDataGenerators
     train_img_gen = ImageDataGenerator(rescale=1.0/255.0, preprocessing_function=remove_text)
@@ -90,6 +77,48 @@ def train_model(cfg, data, model, callbacks, verbose=1):
     test_generator = test_img_gen.flow_from_dataframe(dataframe=data['TEST'], directory=cfg['PATHS']['TEST_IMGS'],
         x_col="filename", y_col=y_col, target_size=img_shape, batch_size=cfg['TRAIN']['BATCH_SIZE'], class_mode=class_mode,
         shuffle=False)
+
+    # Apply class imbalance strategy. We have many more X-rays negative for COVID-19 than positive.
+    class_weight = None
+    if cfg['TRAIN']['IMB_STRATEGY'] == 'class_weight':
+        class_multiplier = None
+        if cfg['TRAIN']['CLASS_MODE'] == 'binary':
+            histogram = np.bincount(data['TRAIN']['label'].astype(int))
+        else:
+            histogram = np.bincount(np.array(train_generator.labels).astype(int))
+            class_multiplier = cfg['TRAIN']['CLASS_MULTIPLIER']
+        class_weight = get_class_weights(histogram, class_multiplier)
+    else:
+        data['TRAIN'] = random_minority_oversample(data['TRAIN'])
+
+    # Define metrics.
+    thresholds = cfg['TRAIN']['THRESHOLDS']     # Load classification thresholds
+    if cfg['TRAIN']['CLASS_MODE'] == 'binary':
+        metrics = ['accuracy', BinaryAccuracy(name='accuracy'),
+                   Precision(name='precision', thresholds=thresholds),
+                   Recall(name='recall', thresholds=thresholds),
+                   AUC(name='auc'),
+                   F1Score(name='f1score', thresholds=thresholds)]
+    else:
+        covid_class_idx = test_generator.class_indices['COVID-19']   # Get index of COVID-19 class
+        thresholds = 1.0 / len(cfg['DATA']['CLASSES'])
+        metrics = ['accuracy', BinaryAccuracy(name='accuracy'),
+                   Precision(name='precision', thresholds=thresholds, class_id=covid_class_idx),
+                   Recall(name='recall', thresholds=thresholds, class_id=covid_class_idx),
+                   AUC(name='auc'),
+                   F1Score(name='f1score', thresholds=thresholds, class_id=covid_class_idx)]
+
+    # Define the model.
+    histogram = list(np.bincount(data['TRAIN']['label'].astype(int)))
+    print(['Class ' + str(i) + ': ' + str(histogram[i]) + '. ' for i in range(len(histogram))])
+    input_shape = cfg['DATA']['IMG_DIM'] + [3]
+    if cfg['TRAIN']['CLASS_MODE'] == 'binary':
+        output_bias = np.log([histogram[1] / histogram[0]])
+        model = dcnn_binary(cfg['NN']['DCNN_BINARY'], input_shape, metrics, output_bias=output_bias)
+    else:
+        n_classes = len(cfg['DATA']['CLASSES'])
+        model = dcnn_multiclass(cfg['NN']['DCNN_MULTICLASS'], input_shape, n_classes, metrics)
+
 
     # Train the model.
     steps_per_epoch = ceil(train_generator.n / train_generator.batch_size)
@@ -128,18 +157,6 @@ def train_experiment(experiment='single_train', save_weights=True, write_logs=Tr
     data['VAL'] = pd.read_csv(cfg['PATHS']['VAL_SET'])
     data['TEST'] = pd.read_csv(cfg['PATHS']['TEST_SET'])
 
-    # Define metrics.
-    thresholds = cfg['TRAIN']['THRESHOLDS']     # Load classification thresholds
-    if cfg['TRAIN']['CLASS_MODE'] == 'binary':
-        covid_class_id = None
-    else:
-        covid_class_id = 1
-    metrics = ['accuracy', BinaryAccuracy(name='accuracy'),
-               Precision(name='precision', thresholds=thresholds, class_id=covid_class_id),
-               Recall(name='recall', thresholds=thresholds, class_id=covid_class_id),
-               AUC(name='auc'),
-               F1Score(name='f1score', thresholds=thresholds, class_id=covid_class_id)]
-
     # Set callbacks.
     early_stopping = EarlyStopping(monitor='val_loss', verbose=1, patience=cfg['TRAIN']['PATIENCE'], mode='min', restore_best_weights=True)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10, min_lr=0.00001, verbose=1)
@@ -149,30 +166,25 @@ def train_experiment(experiment='single_train', save_weights=True, write_logs=Tr
         tensorboard = TensorBoard(log_dir=log_dir, histogram_freq=1)
         callbacks.append(tensorboard)
 
-    # Define the model.
-    histogram = list(np.bincount(data['TRAIN']['label'].astype(int)))
-    print(['Class ' + str(i) + ': ' + str(histogram[i]) + '. ' for i in range(len(histogram))])
-    input_shape = cfg['DATA']['IMG_DIM'] + [3]
-    if cfg['TRAIN']['CLASS_MODE'] == 'binary':
-        output_bias = np.log([histogram[1] / histogram[0]])
-        model = dcnn_binary(cfg['NN']['DCNN_BINARY'], input_shape, metrics, output_bias=output_bias)
-    else:
-        n_classes = len(cfg['DATA']['CLASSES'])
-        model = dcnn_multiclass(cfg['NN']['DCNN_MULTICLASS'], input_shape, n_classes, metrics)
-
     # Conduct desired train experiment
     if experiment == 'single_train':
-        model, test_metrics, test_generator = train_model(cfg, data, model, callbacks)
+        model, test_metrics, test_generator = train_model(cfg, data, callbacks)
 
     # Visualization of test results
     test_predictions = model.predict_generator(test_generator, verbose=0)
-    roc_img = plot_roc("Test set", data['TEST']['label'], test_predictions, dir_path=None)
-    cm_img = plot_confusion_matrix(data['TEST']['label'], test_predictions, dir_path=None)
+    test_labels = test_generator.labels
+    if cfg['TRAIN']['CLASS_MODE'] == 'binary':
+        covid_idx = 1
+    else:
+        covid_idx = test_generator.class_indices['COVID-19']
+    roc_img = plot_roc("Test set", test_labels, test_predictions, class_id=covid_idx, dir_path=None)
+    cm_img = plot_confusion_matrix(test_labels, test_predictions, class_id=covid_idx, dir_path=None)
 
     # Log test set results and plots in TensorBoard
     if write_logs:
         writer = tf.summary.create_file_writer(logdir=log_dir)
         test_summary_str = [['**Metric**','**Value**']]
+        thresholds = cfg['TRAIN']['THRESHOLDS']  # Load classification thresholds
         for metric in test_metrics:
             if metric in ['precision', 'recall'] and isinstance(metric, list):
                 metric_values = dict(zip(thresholds, test_metrics[metric]))

src/visualization/visualize.py
@@ -53,18 +53,21 @@ def plot_metrics(history, metrics, dir_path=None):
         plt.savefig(dir_path + 'metrics_' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '.png')
     return
 
-def plot_roc(name, labels, predictions, class_id=0, dir_path=None):
+def plot_roc(name, labels, predictions, class_id=1, dir_path=None):
     '''
     Plots the ROC curve for predictions on a dataset
     :param name: Name of dataset on the plot
     :param labels: Ground truth labels
     :param predictions: Model predictions corresponding to the labels
+    :param class_id: Index of class to consider
     :param dir_path: Directory in which to save image
     '''
     plt.clf()
     if np.max(labels) > 1:
-        predictions = (np.argmax(predictions, axis=1) == class_id) * 1.0    # Only care about one class
-        labels = (labels == class_id) * 1.0
+        single_class_preds = predictions[:, class_id]    # Only care about one class
+        single_class_labels = (np.array(labels) == class_id) * 1.0
+        predictions = single_class_preds
+        labels = single_class_labels
     fp, tp, _ = roc_curve(labels, predictions)  # Get values for true positive and true negative
     plt.plot(100*fp, 100*tp, label=name, linewidth=2)   # Plot the ROC curve
     plt.xlabel('False positives [%]')
@@ -78,18 +81,20 @@ def plot_roc(name, labels, predictions, class_id=0, dir_path=None):
         plt.savefig(dir_path + 'ROC_' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '.png')
     return plot_to_tensor()
 
-def plot_confusion_matrix(labels, predictions, class_id=0, p=0.5, dir_path=None):
+def plot_confusion_matrix(labels, predictions, class_id=1, p=0.5, dir_path=None):
     '''
     Plot a confusion matrix for the ground truth labels and corresponding model predictions.
     :param labels: Ground truth labels
     :param predictions: Model predictions
+    :param class_id: Index of class to consider
     :param p: Classification threshold
     :param dir_path: Directory in which to save image
     '''
     plt.clf()
     if np.max(labels) > 1:
-        single_class_preds = (np.argmax(predictions, axis=1) == class_id) * 1.0    # Only care about one class
-        single_class_labels = (labels == class_id) * 1.0
+        p = 1.0 / np.unique(labels).shape[0]
+        single_class_preds = predictions[:, class_id]    # Only care about one class
+        single_class_labels = (np.array(labels) == class_id) * 1.0
         predictions = single_class_preds
         labels = single_class_labels
     ax = plt.subplot()
@@ -120,7 +125,7 @@ def plot_confusion_matrix(labels, predictions, class_id=0, p=0.5, dir_path=None)
     return plot_to_tensor()
 
 
-def visualize_explanation(orig_img, explanation, img_filename, label, probs, label_to_see='top', file_path=None):
+def visualize_explanation(orig_img, explanation, img_filename, label, probs, class_names, label_to_see='top', file_path=None):
     '''
     Visualize an explanation for the prediction of a single X-ray image.
     :param orig_img: Original X-Ray image
@@ -128,6 +133,7 @@ def visualize_explanation(orig_img, explanation, img_filename, label, probs, lab
     :param img_filename: Filename of the image explained
     :param label: Ground truth class of the example
     :param probs: Prediction probabilities
+    :param class_names: Ordered list of class names
     :param label_to_see: Label to visualize in explanation
     :param file_path: Path to save the generated image
     '''
@@ -144,9 +150,11 @@ def visualize_explanation(orig_img, explanation, img_filename, label, probs, lab
     ax[1].imshow(mark_boundaries(temp, mask))
 
     # Display some information about the example
+    pred_class = np.argmax(probs)
     fig.text(0.02, 0.8, "Prediction probabilities: " + str(['{:.2f}'.format(probs[i]) for i in range(len(probs))]),
              fontsize=10)
-    fig.text(0.02, 0.82, "Ground Truth: " + str(label), fontsize=10)
+    fig.text(0.02, 0.82, "Predicted Class: " + str(pred_class) + ' (' + class_names[pred_class] + ')', fontsize=10)
+    fig.text(0.02, 0.84, "Ground Truth Class: " + str(label) + ' (' + class_names[label] + ')', fontsize=10)
     fig.suptitle("LIME Explanation for image " + img_filename, fontsize=15)
     fig.tight_layout()
 